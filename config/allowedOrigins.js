7aa1ff375e06d6840b245c635e5ce648bb41f178
Pavel Martinez <pavel.martinez.22@gmail.com>
2026-05-13 01:03:40 -0500

    Added Website to allowedOrigins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
   "",
   "https://react-states-app.onrender.com",
+  "https://pavel-portfolio-ws.onrender.com",
   "http://127.0.0.1:5500",
   "http://localhost:3500",
   "http://localhost:3000",
